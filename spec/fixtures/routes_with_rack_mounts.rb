4d496128dbc6c3f4a05e1dadc27862677b8fb6e4
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2012-02-22 16:31:17 -0700

    Comments about mounted Rack apps.

--- spec/fixtures/routes_with_rack_mounts.rb
+++ spec/fixtures/routes_with_rack_mounts.rb
@@ -1,13 +1,19 @@
-Concourse::Application.routes.draw do
+YourApp::Application.routes.draw do
 
+  resources :users, only: [:update]
+  
   match '/facebook/:action'       => 'facebook', as: 'facebook'
 
   if Rails.env.development?
-    mount Preview::ErrorMailer => 'mail_view/error_mailer' # Use this to test emails in browser, go to root/mail_view
+    # These are little Rack apps we defined somewhere in our Rails app.
+    # For our purposes here, we don't care what they do, we just have to define
+    # #call on them to statisfy the router.
+    # 
+    # See the mail_view gem for previewing your mailer views in the browser.
+    mount Preview::ErrorMailer => 'mail_view/error_mailer' 
     mount Preview::UserMailer  => 'mail_view/user_mailer'
     mount Preview::StoreMailer  => 'mail_view/store_mailer'
     mount Preview::BrandMailer  => 'mail_view/brand_mailer'
   end
 
-  resources :users, only: [:update]
 end
